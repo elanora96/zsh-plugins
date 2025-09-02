ebd1f572cf3fd4a5968ca73fab4391a901ff2d9f
Elanora Manson <git@elanora.lol>
2025-09-02 13:23:03 -0700

    User Agent updates with Firefox installed

--- wget/wget.plugin.zsh
+++ wget/wget.plugin.zsh
@@ -2,9 +2,13 @@
 # -mpckE == mirror page-requisites continue konvert Extension
 # Recursively traverses the whole site, downloads all static assets, and converts pathnames 
 function wget-archive() {
+  # Get the current Firefox Version number so this script stays relevant
+  FFV=$(firefox --version | awk '{print $3}')
+  UA="Mozilla/5.0 (X11; Linux x86_64; rv:$FFV) Gecko/20100101 Firefox/$FFV"
+
   wget \
     -mpckE \
-    --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0" \
+    --user-agent="$UA" \
     -e robots=off \
     --wait 2 \
     "$@"
